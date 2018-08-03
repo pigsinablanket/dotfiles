8b9b84b372f198ac77c7107860a7d549566125e9
Daniel Reimer <daniel.k.reimer97@gmail.com>
2018-08-03 12:59:03 -0700

    change fish layout, update xmobar config

---
+++ config.fish
@@ -0,0 +1,10 @@
+set PATH /home/dan/.local/bin $PATH
+
+alias emacs 'emacs -nw'
+alias e 'emacs -nw'
+alias f 'fg'
+alias cp 'cp -v'
+alias mv 'mv -v'
+alias rm 'rm -v'
+
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
