e70d82d760578c9b78379cfb1aaea0842c2098d4
Daniel Reimer <daniel.k.reimer97@gmail.com>
2019-01-30 11:27:52 -0800

    Include git aliases

--- config.fish
+++ config.fish
@@ -1,10 +1,23 @@
 set PATH /home/dan/.local/bin $PATH
 
 alias emacs 'emacs -nw'
-alias e 'emacs -nw'
-alias f 'fg'
-alias cp 'cp -v'
-alias mv 'mv -v'
-alias rm 'rm -v'
+alias e "emacs -nw"
+alias f "fg"
+alias mv "mv -v"
+alias rm "rm -v"
+alias ln "ln -sv"
+alias cp "cp -v"
 
-export DOTNET_CLI_TELEMETRY_OPTOUT=1
+alias g  "git"
+alias gl "git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gs "git status -sb"
+alias ga "git add"
+alias gb "git branch"
+alias gr "git rebase"
+alias gm "git merge"
+alias gc "git commit"
+alias gd "git diff"
+alias gg "git grep"
+alias gp "git push"
+alias go "git checkout"
+
